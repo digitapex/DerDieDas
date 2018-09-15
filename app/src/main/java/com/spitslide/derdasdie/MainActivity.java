e3e63c416c242543f3399c0b8a98280cbddd9898
Ubuntu <spitslide@gmail.com>
2018-09-15 22:35:00 +0200

    Build layout

--- app/src/main/java/com/spitslide/derdasdie/MainActivity.java
+++ app/src/main/java/com/spitslide/derdasdie/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         String[] nouns = FileUtils.getLines(listNouns);
+
         setContentView(R.layout.activity_main);
     }
 }

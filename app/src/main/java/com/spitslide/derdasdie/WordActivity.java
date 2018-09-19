9741e44382a7d5ea7ec1d21a7f4667f293e2cf30
Ubuntu <spitslide@gmail.com>
2018-09-19 17:31:41 +0200

    Replace WordActivity with WordFragment

--- app/src/main/java/com/spitslide/derdasdie/WordActivity.java
+++ app/src/main/java/com/spitslide/derdasdie/WordActivity.java
@@ -10,47 +10,25 @@ import java.util.Random;
 
 public class WordActivity extends ThemedActivity {
 
-    private String correctGender;
-    private TextView nounView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word);
-        nounView = findViewById(R.id.nounView);
-        String listNouns = null;
-        try {
-            listNouns = FileUtils.getNounList(this);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        String[] nouns = FileUtils.getLines(listNouns);
-        String randomNoun = nouns[new Random().nextInt(100)];
-        String noun = randomNoun.split(",")[0];
-        correctGender = randomNoun.split(",")[1];
-        nounView.setText(noun);
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.frame, new WordFragment())
+                .commit();
+
+
+
+
+
+
+
 
     }
 
-    public void onButton(View view) {
-        Button pressedButton = (Button) view;
-        String pressedButtonGender = getResources().getResourceEntryName(view.getId());
-        if (pressedButtonGender.equals(correctGender)) {
-//            nounView.setText(GenderConvert.toFullGender(correctGender) + " " + nounView.getText());
-            pressedButton.setBackgroundResource(R.drawable.button_correct);
-//            pressedButton.setTextColor(ThemeUtil.getPressedButtonTxtColorAttr(this));
-            AnimationUtil.animateJumpAndSlide(this, nounView, true);
-        } else {
-            int idResource = getResources().getIdentifier(correctGender, "id", getPackageName());
-            Button correctButton = findViewById(idResource);
-            AnimationUtil.animateButtonDrawable(this, correctButton);
-            // animating text color not working on real device, but works in emulator, so better to just animate background
-//            AnimationUtil.animateTextColor(correctButton, this);
-            pressedButton.setBackgroundResource(R.drawable.button_wrong);
-            AnimationUtil.animateJumpAndSlide(this, nounView, false);
-//            pressedButton.setTextColor(ThemeUtil.getPressedButtonTxtColorAttr(this));
 
-
-        }
-    }
 }
---
+++ app/src/main/java/com/spitslide/derdasdie/WordFragment.java
@@ -0,0 +1,78 @@
+package com.spitslide.derdasdie;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
+public class WordFragment extends Fragment implements View.OnClickListener {
+
+    private String correctGender;
+    TextView nounView;
+    private View v;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_word, container, false);
+        nounView = v.findViewById(R.id.nounView);
+        v.findViewById(R.id.m).setOnClickListener(this);
+        v.findViewById(R.id.n).setOnClickListener(this);
+        v.findViewById(R.id.f).setOnClickListener(this);
+        return v;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // TODO - what happens without this
+        super.onViewCreated(view, savedInstanceState);
+
+        String listNouns = null;
+        try {
+            listNouns = FileUtils.getNounList(getActivity());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String[] nouns = FileUtils.getLines(listNouns);
+        String randomNoun = nouns[new Random().nextInt(100)];
+        String noun = randomNoun.split(",")[0];
+        correctGender = randomNoun.split(",")[1];
+        nounView.setText(noun);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button pressedButton = (Button) view;
+        String pressedButtonGender = getResources().getResourceEntryName(view.getId());
+        if (pressedButtonGender.equals(correctGender)) {
+//            nounView.setText(GenderConvert.toFullGender(correctGender) + " " + nounView.getText());
+            pressedButton.setBackgroundResource(R.drawable.button_correct);
+//            pressedButton.setTextColor(ThemeUtil.getPressedButtonTxtColorAttr(this));
+            AnimationUtil.animateJumpAndSlide(getActivity(), nounView, true);
+        } else {
+            int idResource = getResources().getIdentifier(correctGender, "id", getActivity().getPackageName());
+            Button correctButton = v.findViewById(idResource);
+            AnimationUtil.animateButtonDrawable(getActivity(), correctButton);
+            // animating text color not working on real device, but works in emulator, so better to just animate background
+//            AnimationUtil.animateTextColor(correctButton, this);
+            pressedButton.setBackgroundResource(R.drawable.button_wrong);
+            AnimationUtil.animateJumpAndSlide(getActivity(), nounView, false);
+//            pressedButton.setTextColor(ThemeUtil.getPressedButtonTxtColorAttr(this));
+
+
+        }
+    }
+}

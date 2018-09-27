157ea2d8ad6749da7565e92f8387f45f3b9cfc8a
Ubuntu <spitslide@gmail.com>
2018-09-27 21:51:21 +0200

    Add simple stats screen

--- app/src/main/java/com/spitslide/derdasdie/StatsActivity.java
+++ app/src/main/java/com/spitslide/derdasdie/StatsActivity.java
@@ -1,82 +1,38 @@
 package com.spitslide.derdasdie;
 
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-
-import java.io.UnsupportedEncodingException;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
 
 public class StatsActivity extends ThemedActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stats);
-
-        try {
-            setupChart();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        setContentView(R.layout.activity_stats_simple);
     }
 
-    private void setupChart() throws UnsupportedEncodingException {
-        int nounsCount = FileUtils.getNounsCount(this);
-        LineChart chart = findViewById(R.id.chart);
-        List<Entry> entries = new ArrayList<>();
-        List<Float> scores = new DatabaseUtil(this).getAllScores();
-        // TODO - if 0 entries
-        for (int i = 0; i < scores.size(); i++) {
-            float percent = scores.get(i) / nounsCount;
-            entries.add(new Entry(i, percent));
-        }
 
 
-        LineDataSet dataSet = new LineDataSet(entries, "");
-        dataSet.setDrawCircles(false);
-
-        LineData lineData = new LineData(dataSet);
-        lineData.setDrawValues(false);
-        chart.setData(lineData);
-
-        chart.getAxisRight().setEnabled(false);
-
-        chart.getAxisLeft().setDrawGridLines(true);
-        chart.getAxisLeft().setAxisMinimum(0);
-
-        // prevents from showing 105 %
-        chart.getAxisLeft().setSpaceTop(5);
-        chart.getAxisLeft().setTextColor(ThemeUtil.getNormalButtonTxtColorAttr(this));
-        chart.getAxisLeft().setGridColor(Color.RED);
-        chart.getAxisLeft().setDrawZeroLine(true);
-        chart.getAxisLeft().setLabelCount(20);
-        chart.getAxisLeft().setValueFormatter(new IAxisValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                DecimalFormat df = new DecimalFormat();
-                df.setMaximumFractionDigits(4);
-                return df.format(value) + " %";
-            }
-        });
-
-        chart.getXAxis().setDrawGridLines(false);
-        chart.getXAxis().setDrawAxisLine(true);
-        chart.getXAxis().setDrawLabels(false);
-        chart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-
-        chart.setDescription(null);
-        chart.getLegend().setEnabled(false);
+    private void showDialog(int title, int text) {
+        String titleString = getResources().getString(title);
+        String textString = getResources().getString(text);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(titleString);
+        builder.setMessage(textString);
+        builder.setPositiveButton("OK", null);
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
+
+    public void onHalfWords(View view) {
+        showDialog(R.string.half_words_title, R.string.half_words_text);
+    }
+
+    public void onFullWords(View view) {
+        showDialog(R.string.full_words_title, R.string.full_words_text);
+    }
+
+
 }

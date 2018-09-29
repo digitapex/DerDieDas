71e9465db2468da2293a801f99df77cb4d7f6c2f
Ubuntu <spitslide@gmail.com>
2018-09-29 11:42:56 +0200

    Fix switch preference

--- app/src/main/java/com/spitslide/derdasdie/ui/SettingsFragment.java
+++ app/src/main/java/com/spitslide/derdasdie/ui/SettingsFragment.java
@@ -6,6 +6,7 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
 import com.spitslide.derdasdie.R;
+import com.spitslide.derdasdie.utils.PrefsUtil;
 import com.spitslide.derdasdie.utils.ThemeUtil;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -13,8 +14,11 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
-        final Preference fontPreference = getPreferenceScreen().findPreference("theme");
-        fontPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+        final Preference themePreference = getPreferenceScreen().findPreference("theme");
+        if (PrefsUtil.getPrefs(getActivity(), "theme")) {
+            themePreference.setSummary("Dark theme");
+        }
+        themePreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             public boolean onPreferenceChange(Preference preference, Object o) {
                 ThemeUtil.setThemeChanged(true);
                 getActivity().recreate();
